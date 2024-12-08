915f1386e03ac606c338835a9901f6e2e3f8753f
Rawr <xuanlockun594@gmail.com>
2024-12-08 22:53:35 +0700

    update demo_web

--- web/main.py
+++ web/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import joblib
 import pandas as pd
 
@@ -14,7 +14,11 @@ def predict():
     data = {key: float(value) for key, value in request.form.items()}
     df = pd.DataFrame([data])
     prediction = model.predict(df)
-    return render_template('result.html', prediction=prediction[0])
+    # return render_template('result.html', prediction=prediction[0])
+    prediction_result = int(prediction[0]) # gán lại type thôi á
+    return jsonify({'prediction': prediction_result})
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
